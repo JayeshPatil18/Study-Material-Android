a2587f583f2a4518bd849ffe99f44ebbdce282aa
Jayesh Patil <jp7470484@gmail.com>
2024-02-19 19:39:29 +0530

    done

---
+++ app/src/main/java/com/apps/notesapp/MainActivity2.java
@@ -0,0 +1,73 @@
+package com.apps.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    CardView card1, card2, card3, card4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        card1 = findViewById(R.id.card1);
+        card2 = findViewById(R.id.card2);
+        card3 = findViewById(R.id.card3);
+        card4 = findViewById(R.id.card4);
+
+        String tech = getIntent().getExtras().getString("tech");
+
+        getSupportActionBar().setTitle(tech.substring(0, 1).toUpperCase() + tech.substring(1));
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                intent.putExtra("tech", tech);
+                intent.putExtra("res", "notes");
+                startActivity(intent);
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                intent.putExtra("tech", tech);
+                intent.putExtra("res", "books");
+                startActivity(intent);
+            }
+        });
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                intent.putExtra("tech", tech);
+                intent.putExtra("res", "questions");
+                startActivity(intent);
+            }
+        });
+
+        card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                intent.putExtra("tech", tech);
+                intent.putExtra("res", "documentation");
+                startActivity(intent);
+            }
+        });
+    }
+}
